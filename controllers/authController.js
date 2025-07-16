5248e17bb68b51f03d607a6b72c4039306c9eb84
Suneel Mekala <132973483+Suneel-Sweety-M@users.noreply.github.com>
2025-07-16 12:14:35 +0530

    Update authController.js
    Auth Logout Changed

--- controllers/authController.js
+++ controllers/authController.js
@@ -214,8 +214,9 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
   res.clearCookie("refreshToken", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    // secure: process.env.NODE_ENV === "production",
+    secure: true,
+    sameSite: "None",
   });
   return res
     .status(200)
